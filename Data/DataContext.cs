886c0f84316cf12da05ec6cddd4383d60040ba3d
Kevin Alexander Arevalo <Kaas7520@gmail.com>
2022-10-27 21:52:00 -0500

    Agregar archivos de proyecto.

---
+++ Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticaViamatica.Model;
+
+namespace PracticaViamatica.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public  DbSet<Producto> producto { get; set; }
+    }
+}
---
+++ Model/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticaViamatica.Model
+{
+    public class Producto
+    {
+        [Key]
+        public int idProducto { get; set; }
+        [Required]
+        public string? descripcion { get; set; }
+        [Required]
+        public double precio { get; set; }
+        [Required]
+        public int cantidadDisponible { get; set; }
+    }
+}
